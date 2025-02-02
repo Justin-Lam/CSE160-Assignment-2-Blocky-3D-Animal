498e03abc92e6050d020558a71b3340e2ed2aa5c
Justin Lam <justinflam9225k@gmail.com>
2025-02-01 22:05:22 -0800

    Added animation buttons

--- src/asg2.js
+++ src/asg2.js
@@ -62,10 +62,14 @@ function compileShadersAndConnectVariables() {
 
 let g_globalRotation = 0;
 let g_headRotation = 0;
+let g_animation_enabled_head = false;
+let g_tongueRotation = 0;
+let g_animation_enabled_tongue = false;
 let g_leg_front_leftRotation = 0;
 let g_leg_front_rightRotation = 0;
 let g_leg_back_leftRotation = 0;
 let g_leg_back_rightRotation = 0;
+let g_animation_enabled_legs = false;
 
 function createUIEvents() {
 	document.getElementById("globalRotationSlider").addEventListener("mousemove", function() {
@@ -77,6 +81,12 @@ function createUIEvents() {
 		g_headRotation = this.value;
 		renderAllShapes();
 	});
+	document.getElementById("toggleAnimationButton_Head").onclick = () => g_animation_enabled_head = !g_animation_enabled_head;
+	document.getElementById("tongueRotationSlider").addEventListener("mousemove", function() {
+		g_tongueRotation = this.value;
+		renderAllShapes();
+	});
+	document.getElementById("toggleAnimationButton_Tongue").onclick = () => g_animation_enabled_tongue = !g_animation_enabled_tongue;
 
 	document.getElementById("leg_front_leftRotationSlider").addEventListener("mousemove", function() {
 		g_leg_front_leftRotation = this.value;
@@ -94,6 +104,7 @@ function createUIEvents() {
 		g_leg_back_rightRotation = this.value;
 		renderAllShapes();
 	});
+	document.getElementById("toggleAnimationButton_Legs").onclick = () => g_animation_enabled_legs = !g_animation_enabled_legs;
 }
 
 function main() {
@@ -101,7 +112,34 @@ function main() {
 	compileShadersAndConnectVariables();
 	createUIEvents();
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	requestAnimationFrame(tick);
+}
+
+const g_startTime = currentTime();
+let g_elapsedTime = 0;
+function currentTime() {
+	return performance.now() / 1000.0;
+}
+function tick() {
+	g_elapsedTime = currentTime() - g_startTime;
+	updateAnimationAngles();
 	renderAllShapes();
+	requestAnimationFrame(tick);
+}
+
+function updateAnimationAngles() {
+	if (g_animation_enabled_head) {
+		g_headRotation = 45 * Math.sin(g_elapsedTime);
+	}
+	if (g_animation_enabled_tongue) {
+		g_tongueRotation = 30 * Math.sin(g_elapsedTime * 20);
+	}
+	if (g_animation_enabled_legs) {
+		g_leg_front_leftRotation = 45 * Math.sin(g_elapsedTime * 4);
+		g_leg_back_leftRotation = 45 * Math.sin(g_elapsedTime * 4);
+		g_leg_front_rightRotation = 45 * Math.sin(g_elapsedTime * 4 + Math.PI);
+		g_leg_back_rightRotation = 45 * Math.sin(g_elapsedTime * 4 + Math.PI);
+	}
 }
 
 function renderAllShapes() {
@@ -152,27 +190,33 @@ function renderAllShapes() {
 	const head = new Cube();
 	head.color = [1, 0.2, 0.8, 1];	// pink
 	head.matrix.translate(0, 0.15, -0.3);
-	head.matrix.rotate(-g_headRotation, 0, 0, 1);
+	head.matrix.rotate(g_headRotation, 0, 0, 1);
+	let headCoordsMatrix = new Matrix4(head.matrix);
 	head.matrix.scale(0.4, 0.4, 0.4);
 	head.matrix.translate(-0.5, -0.5, 0);
 	head.render();
 
 	const eye_left = new Cube();
 	eye_left.color = [0, 0, 0, 1];	// black
-	eye_left.matrix.translate(0.05, 0.15, -0.325);
+	eye_left.matrix = new Matrix4(headCoordsMatrix);
+	eye_left.matrix.translate(0.05, 0, -0.025);
 	eye_left.matrix.scale(0.1, 0.1, 0.1);
 	eye_left.render();
 
 	const eye_right = new Cube();
 	eye_right.color = [0, 0, 0, 1];	// black
-	eye_right.matrix.translate(-0.15, 0.15, -0.325);
+	eye_right.matrix = new Matrix4(headCoordsMatrix);
+	eye_right.matrix.translate(-0.15, 0, -0.025);
 	eye_right.matrix.scale(0.1, 0.1, 0.1);
 	eye_right.render();
 
 	const tongue = new Cube();
 	tongue.color = [1, 0, 0, 1];	// red
-	tongue.matrix.translate(-0.1, 0.0, -0.45);
+	tongue.matrix = new Matrix4(headCoordsMatrix);
+	tongue.matrix.translate(0, -0.15, 0.025);
+	tongue.matrix.rotate(g_tongueRotation, 1, 0, 0);
 	tongue.matrix.rotate(60, 1, 0, 0);
-	tongue.matrix.scale(0.2, 0.2, 0.1);
+	tongue.matrix.scale(0.2, 0.2, 0.05);
+	tongue.matrix.translate(-0.5, -1, -0.5);
 	tongue.render();
 }
